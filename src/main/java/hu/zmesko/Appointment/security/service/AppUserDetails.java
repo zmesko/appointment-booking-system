0b7a93dddef4f041a4b4a9cf8ce73cdd1bde4211
zmesko <zmesi66@gmail.com>
2026-01-24 16:32:13 +0100

    preparing for AWS RDS

--- src/main/java/hu/zmesko/Appointment/security/service/AppUserDetails.java
+++ src/main/java/hu/zmesko/Appointment/security/service/AppUserDetails.java
@@ -20,7 +20,7 @@ public class AppUserDetails implements UserDetails {
         this.userName = appUser.getUsername();
         this.password = appUser.getPassword();
         this.authorities = appUser.getRoles() == null ? List.of()
-                : List.of(appUser.getRoles().split(","))
+                : List.of(appUser.getRoles().split(", "))
                         .stream().map(SimpleGrantedAuthority::new)
                         .collect(Collectors.toList());
     }
